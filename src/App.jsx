cc2693b402c3f06843de57b143e1c3808c8bc05b
Thanate Dokmai <nook.microstock@gmail.com>
2024-10-23 14:38:42 +0700

    feate:react-form-handling-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,39 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [greetingMsg, setGreetingMsg] = useState("Greeting Message");
+  const [inputText, setInputText] = useState("");
+
+  
+  const handleGreetingChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  
+  const handleGreetingUpdate = () => {
+    setGreetingMsg(inputText);
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMsg}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          value={inputText}
+          onChange={handleGreetingChange} 
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleGreetingUpdate}>Update text</button> 
       </div>
     </div>
   );
 }
 
 export default App;
+
