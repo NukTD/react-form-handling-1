0eb6bbb5ad68e8594cd401832411634da73f1842
TheYami <luffy90san@gmail.com>
2024-10-23 13:21:33 +0700

    feat: Add onChange and onClick to input and button for change display greeting

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+
+  const [greetingMsg, setGreetingMsg] = useState('Greeting Message');
+  const [newGreetingMsg, setNewGreetingMsg] = useState('');
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMsg}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input id="greeting-message" type="text" onChange={(e)=>{setNewGreetingMsg(e.target.value)}}/>
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={()=>{setGreetingMsg(newGreetingMsg)}}>Update text</button>
       </div>
     </div>
   );
